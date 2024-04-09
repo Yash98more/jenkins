1e63a6443e3ef75ba4a698a67c7ce3852e033d49
yash <yash@gmail.com>
2024-04-10 02:17:56 +0530

    --- Auto Git Commit ---

---
+++ pipeline_one.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+// Performing mulitple stages for our application
+    stages {
+        stage("pull") {
+                steps {
+                    echo "pull Done"
+                }
+        }
+        stage("Build") {
+                step {
+                    echo "Build Done"
+                }
+        }
+        stage("Quality-Test") {
+                step {
+                    echo "Quality-check Done"
+                }
+        }
+        stage("Deploy") {
+                step {
+                    echo "Deploy Done"
+                }
+        }
+    }
+}
