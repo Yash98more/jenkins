8c8ac954bd082f63c5606be0a7350c69ca072412
yash <yash@gmail.com>
2024-04-10 15:59:36 +0530

    --- Auto Git Commit ---

---
+++ studentapp_ui.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+// #Performing mulitple stages for our application
+    stages {
+        stage("Pull") {
+                steps {
+                    git branch: 'main', changelog: false, poll: false, url: 'https://github.com/Yash98more/studentapp.ui'
+                    echo "yes, Application repository pull is done ! "
+                }
+        }
+        stage("Build") {
+                steps {
+                    sh '/opt/apache-maven-3.9.6/bin/mvn clean package'
+                    echo "Build Done"
+                }
+        }
+        stage("Test") {
+			steps {
+			    echo "Test Done"
+			}
+		}
+
+        stage("Quality-Test") {
+                steps {
+                    echo "Quality-check Done"
+                }
+        }
+        stage("Deploy") {
+                steps {
+                    echo "Deploy Done"
+                }
+        }
+    }
+}
